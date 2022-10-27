38c520db187f16fd4ff1dc22e878997e0eeb7ca6
Gabrielegl <gabrielegl@ufcspa.edu.br>
2022-10-26 22:42:37 -0300

    added project

---
+++ backend/cadastro/cadastro.php
@@ -0,0 +1,17 @@
+<?php 
+
+require_once '../conexao.php';
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$login = $_POST['login'];
+$password = base64_encode($_POST['password']) ;
+
+$query = "INSERT INTO `user` (`user_id`, `user_name`, `user_email`, `user_login`, `user_password`) VALUES (NULL, '$nome', '$email', '$login', '$password');";
+$resultado = mysqli_query($conn, $query);
+
+if($conn->insert_id){
+    echo 'Usuário adicionado com sucesso!';
+}
+
+
+
