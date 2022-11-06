dcbee3d0915f857355d57510980d12645ea3e5a1
Gabrielegl <gabrielegl@ufcspa.edu.br>
2022-11-05 22:15:45 -0300

    added changes when adding training

--- deshboard/alunos/alunos.php
+++ deshboard/alunos/alunos.php
@@ -17,12 +17,24 @@
     </div>
     <?php while ($row_linhas = mysqli_fetch_array($resultado)) { ?>
 
-        <div id="nome<?= $count; ?>" class="user"  onclick="teste('<?= $count; ?>')">Nome: <?= $row_linhas["user_name"] ?>
-            <span class="material-symbols-outlined material-symbols-outlined-icon">
-                expand_more
-            </span>
+        <div class="container-adicionar-alunos">
+            <div class="subcontainer-adicionar-alunos">
+                <div id="nome<?= $count; ?>" class="user">Nome: <?= $row_linhas["user_name"] ?>
+                    <div class="material-symbols-outlined-icon">
+                        <span class="material-symbols-outlined" title="Adicionar treino" onclick="adicionarTreino()">
+                            add
+                        </span>
+                        <span id="expand_more" class="material-symbols-outlined" onclick="teste('<?= $count; ?>')">
+                            expand_more
+                        </span>
+                    </div>
+                </div>
+                <div id="email<?= $count; ?>" class="linha-user-email">Email: <?= $row_linhas["user_email"] ?> </div>
+            </div>
+            <div id="container-treinos">
+
+            </div>
         </div>
-        <div id="email<?= $count; ?>" class="linha-user-email">Email: <?= $row_linhas["user_email"] ?> </div>
 
         <?php $count++; ?>
     <?php  } ?>
@@ -35,22 +47,31 @@
         if ($("#email" + count).hasClass("linha-user-email")) {
             $("#email" + count).removeClass("linha-user-email");
             $("#email" + count).addClass("linha-user-email-block");
-            $("#nome" + count).remove("user");
-            $("#nome" + count).addClass("user-selected");
-            do {
-                count++;
-                $("#nome" + count).css("display", "none")
-            } while (count <= count_delete);
+
+            // do {
+            //     count++;
+            //     $("#nome" + count).css("display", "none")
+            // } while (count <= count_delete);
         } else {
             $("#email" + count).removeClass("linha-user-email-block");
             $("#email" + count).addClass("linha-user-email");
+
+            // do {
+            //     count++;
+            //     $("#nome" + count).css("display", "flex")
+            // } while (count <= count_delete);
+        }
+        if ($("#nome" + count).hasClass("user")) {
+            $("#nome" + count).remove("user");
+            $("#nome" + count).addClass("user-selected");
+        } else {
             $("#nome" + count).addClass("user")
             $("#nome" + count).removeClass("user-selected")
-            do {
-                count++;
-                $("#nome" + count).css("display", "block")
-            } while (count <= count_delete);
         }
+
     }
 
+    function adicionarTreino() {
+
+    }
 </script>
